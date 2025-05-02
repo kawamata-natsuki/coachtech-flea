26638c1ea06c60c3f3639dde1e8542bd2f687857
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-02 11:55:00 +0900

    feat：メール認証実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,8 +8,17 @@ use App\Http\Controllers\ItemCommentController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\UserController;
 use App\Models\Item;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Log;
+
+Route::get('/log-test', function () {
+    Log::debug('ログテスト成功！');
+    return 'ログ出力OK';
+});
 
 // -----------------------------------------------------
 // 認証関係のルート
@@ -35,11 +44,26 @@ Route::post('/login', function (LoginRequest $request) {
     ])->withInput();
 });
 
+// メール認証
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill(); //認証完了
+    return redirect('/mypage/profile'); //認証後のリダイレクト先
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', '確認メールを再送しました');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 // -----------------------------------------------------
 // プロフィール画面のルート
 // -----------------------------------------------------
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/mypage', [UserController::class, 'index'])->name('profile.index');
     Route::get('/mypage/profile', [UserController::class, 'edit'])->name('profile.edit');
     Route::put('/mypage/profile', [UserController::class, 'update'])->name('profile.update');
@@ -49,7 +73,7 @@ Route::middleware(['auth'])->group(function () {
 // 住所変更画面のルート
 // -----------------------------------------------------
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/purchase/address/{item}', [UserController::class, 'editAddress'])->name('address.edit');
     Route::put('/purchase/address/{item}', [UserController::class, 'updateAddress'])->name('address.update');
 });
@@ -57,7 +81,7 @@ Route::middleware('auth')->group(function () {
 // -----------------------------------------------------
 // 商品出品に関するルート
 // -----------------------------------------------------
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/sell', [ItemController::class, 'create'])->name('items.create');
     Route::post('/sell', [ItemController::class, 'store'])->name('items.store');
 });
@@ -76,19 +100,19 @@ Route::get('/', [ItemController::class, 'index'])->name('items.index');
 Route::get('/item/{item}', [ItemController::class, 'show'])->name('items.show');
 
 // いいね機能
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/item/{item}/favorite', [FavoriteController::class, 'toggle'])->name('item.favorite.toggle');
 });
 
 // コメント機能
-Route::middleware('auth')->post('/items/{item}/comments', [ItemCommentController::class, 'store'])->name('items.comments.store');
+Route::middleware(['auth', 'verified'])->post('/items/{item}/comments', [ItemCommentController::class, 'store'])->name('items.comments.store');
 
 // -----------------------------------------------------
 // 商品購入画面のルーティング
 // -----------------------------------------------------
 
-Route::middleware('auth')->get('/purchase/{item}', [OrderController::class, 'show'])->name('purchase.show');
+Route::middleware(['auth', 'verified'])->get('/purchase/{item}', [OrderController::class, 'show'])->name('purchase.show');
 
-Route::middleware('auth')->post('/purchase/{item}', [OrderController::class, 'store'])->name('purchase.store');
+Route::middleware(['auth', 'verified'])->post('/purchase/{item}', [OrderController::class, 'store'])->name('purchase.store');
 
 Route::get('/purchase/success/{item}', [OrderController::class, 'success'])->name('purchase.success');
