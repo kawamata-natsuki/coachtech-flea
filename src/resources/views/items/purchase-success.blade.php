a08653db1f3def1cc22f9485af8ed197b202de08
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-04 04:17:02 +0900

    style：サンクスページのレイアウト作成、レスポンシブデザイン対応

--- src/resources/views/items/purchase-success.blade.php
+++ src/resources/views/items/purchase-success.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/purchase-success.css') }}">
+@endsection
+
 @section('content')
 <div class="purchase-success">
-  <h1 class="purchase-success__title">ご購入ありがとうございます！</h1>
-  <p class="purchase-success__message">
-    商品のご購入が完了しました。<br>
-    発送までしばらくお待ちください。
-  </p>
+  <div class="purchase-success__container">
+    <h1 class="purchase-success__title">ご購入ありがとうございます！</h1>
+    <p class="purchase-success__message">
+      商品のご購入が完了しました。<br>
+      発送までしばらくお待ちください。
+    </p>
 
-  <div class="purchase-success__actions">
-    <a href="{{ route('items.index') }}" class="btn btn-primary">トップページへ戻る</a>
-    <a href="{{ route('profile.index') }}" class="btn btn-secondary">購入履歴を見る</a>
+    <div class="purchase-success__actions">
+      <a href="{{ route('items.index') }}" class="purchase-success__button purchase-success__button--primary">
+        トップページへ戻る
+      </a>
+      <a href="{{ route('profile.index', ['tab' => 'buy']) }}"
+        class="purchase-success__button purchase-success__button--secondary">
+        購入履歴を見る
+      </a>
+    </div>
   </div>
 </div>
 @endsection
