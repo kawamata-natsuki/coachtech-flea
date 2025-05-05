9455c2771f21f663c41202eee68abd5512fe6487
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-06 00:56:23 +0900

    test：ログアウト機能のテスト

--- src/tests/Feature/Auth/LogoutTest.php
+++ src/tests/Feature/Auth/LogoutTest.php
@@ -2,21 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class LogoutTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    public function test_user_can_logout_successfully()
+    {
+        // 事前にログイン用のユーザーを作成
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password1234'),
+        ]);
+
+        // 1. ユーザーにログインをする
+        $response = $this->get('/login');
         $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => 'test@example.com',
+            'password' => 'password1234',
+        ]);
+        $this->assertAuthenticatedAs($user);
+
+        // 2. ログアウトボタンを押す
+        $response = $this->post('/logout');
+        $response->assertRedirect('/');
+
+        $this->assertGuest();
     }
 }
