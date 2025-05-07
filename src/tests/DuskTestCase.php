374f253b7bd0ba81af023e536369d3ef4b5e4398
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-07 21:58:02 +0900

    test：Duskセットアップ

--- src/tests/DuskTestCase.php
+++ src/tests/DuskTestCase.php
@@ -3,15 +3,31 @@
 namespace Tests;
 
 use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Chrome\ChromeOptions;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public static function prepare()
+    protected function driver()
     {
-        if (! static::runningInSail()) {
-            static::startChromeDriver();
-        }
+        $options = new ChromeOptions();
+        $options->addArguments([
+            '--disable-gpu',
+            '--headless',
+            '--no-sandbox',
+            '--window-size=1920,1080',
+            '--user-data-dir=/tmp/chrome',
+        ]);
+
+        return RemoteWebDriver::create(
+            'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY,
+                $options
+            )
+        );
     }
 }
