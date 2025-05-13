9e39b98f52f9ca06d43b40d9bff4f47a339473e7
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-13 09:05:38 +0900

    refactor：いいねの切り替えをするコードをシンプルに修正

--- src/app/Http/Controllers/FavoriteController.php
+++ src/app/Http/Controllers/FavoriteController.php
@@ -8,14 +8,7 @@ class FavoriteController extends Controller
 {
     public function toggle(Item $item)
     {
-        $user = auth()->user();
-
-        if ($user->favoriteItems()->where('item_id', $item->id)->exists()) {
-            $user->favoriteItems()->detach($item->id); // いいね解除
-        } else {
-            $user->favoriteItems()->attach($item->id); // いいねする
-        }
-
+        auth()->user()->favoriteItems()->toggle($item->id);
         return back();
     }
 }
