d0760299c79e7dec8e0f5a7f95b4daf9f5339b7f
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-13 09:05:07 +0900

    fix：ダミー商品のコンディションを追加

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -36,74 +36,84 @@ class ItemSeeder extends Seeder
                 'brand' => 'EMPORIO ARMANI',
                 'price' => 15000,
                 'description' => 'スタイリッシュなデザインのメンズ腕時計',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Armani+Mens+Clock.jpg',
+                'item_image' => 'images/items/armani-mens-clock.jpg',
+                'condition_code' => 'good',
             ],
             [
                 'name' => 'HDD',
                 'brand' => 'TOSHIBA',
                 'price' => 5000,
                 'description' => '高速で信頼性の高いハードディスク',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/HDD+Hard+Disk.jpg',
+                'item_image' => 'images/items/hdd-hard-disk.jpg',
+                'condition_code' => 'clean',
             ],
             [
                 'name' => '玉ねぎ3束',
                 'brand' => 'IBARAKI FARM',
                 'price' => 300,
                 'description' => '新鮮な玉ねぎ3束のセット',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/iLoveIMG+d.jpg',
+                'item_image' => 'images/items/onion-3-bundles.jpg',
+                'condition_code' => 'used',
             ],
             [
                 'name' => '革靴',
                 'brand' => 'REGAL',
                 'price' => 4000,
                 'description' => 'クラシックなデザインの革靴',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Leather+Shoes+Product+Photo.jpg',
+                'item_image' => 'images/items/leather-shoes.jpg',
+                'condition_code' => 'bad',
             ],
             [
                 'name' => 'ノートPC',
                 'brand' => 'DELL',
                 'price' => 45000,
                 'description' => '高性能なノートパソコン',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Living+Room+Laptop.jpg',
+                'item_image' => 'images/items/silver-laptop.jpg',
+                'condition_code' => 'good',
             ],
             [
                 'name' => 'マイク',
                 'brand' => 'SONY',
                 'price' => 8000,
                 'description' => '高音質のレコーディング用マイク',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Music+Mic+4632231.jpg',
+                'item_image' => 'images/items/studio-mic.jpg',
+                'condition_code' => 'clean',
             ],
             [
                 'name' => 'ショルダーバッグ',
                 'price' => 3500,
                 'description' => 'おしゃれなショルダーバッグ',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Purse+fashion+pocket.jpg',
+                'item_image' => 'images/items/shoulder-bag.jpg',
+                'condition_code' => 'used',
             ],
             [
                 'name' => 'タンブラー',
                 'price' => 500,
                 'description' => '使いやすいタンブラー',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Tumbler+souvenir.jpg',
+                'item_image' => 'images/items/tumbler.jpg',
+                'condition_code' => 'bad',
             ],
             [
                 'name' => 'コーヒーミル',
                 'price' => 4000,
                 'description' => '手動のコーヒーミル',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/Waitress+with+Coffee+Grinder.jpg',
+                'item_image' => 'images/items/coffee-mill.jpg',
+                'condition_code' => 'good',
             ],
             [
                 'name' => 'メイクセット',
                 'price' => 2500,
                 'description' => '便利なメイクアップセット',
-                'item_image' => 'https://coachtech-matter.s3.ap-northeast-1.amazonaws.com/image/%E5%A4%96%E5%87%BA%E3%83%A1%E3%82%A4%E3%82%AF%E3%82%A2%E3%83%83%E3%83%95%E3%82%9A%E3%82%BB%E3%83%83%E3%83%88.jpg',
+                'item_image' => 'images/items/makeup-set.jpg',
+                'condition_code' => 'clean',
             ],
             [
                 'name' => 'CARDCARDCARDCARDCARDCARDCARDCARDCARDCARD',
                 'brand' => 'BRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRANDBRAND',
                 'price' => 9999999,
                 'description' => '美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！美品のトレカです！！！！！！！',
-                'item_image' => 'https://github.com/kawamata-natsuki/coachtech-flea/blob/main/src/public/images/test-item.jpg?raw=true',
-                'attach_all_categories' => true,
+                'item_image' => 'images/items/card-item.jpg',
+                'condition_code' => 'good',
             ]
         ];
 
@@ -118,12 +128,12 @@ class ItemSeeder extends Seeder
             $isSold = $sellerIndex < 6;
 
             // 状態（condition）取得
-            $conditionCode = $faker->randomElement(ConditionConstants::all());
+            $conditionCode = $data['condition_code'] ?? $faker->randomElement(ConditionConstants::all());
             $conditionId = $conditionMap[$conditionCode]->id;
 
             // 画像保存
             $filename = 'items/' . Str::uuid() . '.jpg';
-            $content = file_get_contents($data['item_image']);
+            $content = file_get_contents(base_path('public/' . $data['item_image']));
             Storage::disk('public')->put($filename, $content);
 
             // 商品登録
