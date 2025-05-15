905332f2c4b91c706367db2fd7588edcf58cfd36
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-15 23:41:53 +0900

    chore：レイアウト調整・コード整理

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -5,24 +5,29 @@
 @endsection
 
 @section('content')
-<div class="verify-email">
+<div class="verify-email-page">
   <h1 class="sr-only">メール認証</h1>
-  <p class="verify-email__message">
+  <p class="verify-email-page__message">
     登録していただいたメールアドレスに認証メールを送付しました。<br>
     メール認証を完了してください。
   </p>
 
-  <form class="verify-email__form verify-email__form--confirm" method="GET" action="{{ route('verification.check') }}">
-    <button class="verify-email__confirm-button" type="submit">
+  <!-- 認証確認 -->
+  <form class="verify-email-page__form verify-email-page__form--confirm" method="GET"
+    action="{{ route('verification.check') }}">
+    <button class="verify-email-page__confirm-button" type="submit">
       認証はこちらから
     </button>
   </form>
 
-  <form class="verify-email__form verify-email__form--resend" method="POST" action="{{ route('verification.send') }}">
+  <!-- 認証メール再送信 -->
+  <form class="verify-email-page__form verify-email-page__form--resend" method="POST"
+    action="{{ route('verification.send') }}">
     @csrf
-    <button class="verify-email__resend-button" type="submit">
+    <button class="verify-email-page__resend-button" type="submit">
       認証メールを再送する
     </button>
   </form>
+
 </div>
 @endsection
