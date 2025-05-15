d2ccb58c2ff1c2d34e50edfecafa9c43bf2996d4
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-15 17:07:16 +0900

    chore：レイアウト調整・コード整理

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -5,37 +5,36 @@
 @endsection
 
 @section('content')
-
-<div class="register-form">
-  <div class="register-form__container">
-    <h1 class="register-form__heading content__heading">
+<div class="register-page">
+  <div class="register-page__container">
+    <h1 class="register-page__heading content__heading">
       会員登録
     </h1>
 
-    <div class="register-form__content">
-      <form class="register-form__form" action="/register" method="post" novalidate>
+    <div class="register-page__content">
+      <form class="register-page__form" action="/register" method="post" novalidate>
         @csrf
 
         <!-- ユーザー名 -->
-        <div class="register-form__group">
-          <label class="register-form__label form__label" for="name">ユーザー名</label>
-          <input class="register-form__input form__input" type="text" name="name" id="name" value="{{ old('name') }}"
+        <div class="register-page__form-group">
+          <label class="register-page__label form__label" for="name">ユーザー名</label>
+          <input class="form__input register-page__input" type="text" name="name" id="name" value="{{ old('name') }}"
             placeholder="例：山田　太郎">
           <x-error-message field="name" class="error-message {{ $errors->has('name') ? 'has-error' : 'no-error' }}" />
         </div>
 
         <!-- メールアドレス -->
-        <div class="register-form__group">
-          <label class="register-form__label form__label" for="email">メールアドレス</label>
-          <input class="register-form__input form__input" type="email" name="email" id="email"
+        <div class="register-page__form-group">
+          <label class="register-page__label form__label" for="email">メールアドレス</label>
+          <input class="form__input register-page__input" type="email" name="email" id="email"
             value="{{ old('email') }}" placeholder="例：user@example.com">
           <x-error-message field="email" class="error-message {{ $errors->has('email') ? 'has-error' : 'no-error' }}" />
         </div>
 
         <!-- パスワード -->
-        <div class="register-form__group">
-          <label class="register-form__label form__label" for="password">パスワード</label>
-          <input class="register-form__input form__input" type="password" name="password" id="password"
+        <div class="register-page__form-group">
+          <label class="register-page__label form__label" for="password">パスワード</label>
+          <input class="form__input register-page__input" type="password" name="password" id="password"
             placeholder="8文字以上のパスワードを入力">
           <x-error-message field="password"
             class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
@@ -43,23 +42,23 @@
         </div>
 
         <!-- 確認用パスワード -->
-        <div class="register-form__group">
-          <label class="register-form__label form__label" for="password_confirmation">確認用パスワード</label>
-          <input class="register-form__input form__input" type="password" name="password_confirmation"
+        <div class="register-page__form-group">
+          <label class="register-page__label form__label" for="password_confirmation">確認用パスワード</label>
+          <input class="form__input register-page__input" type="password" name="password_confirmation"
             id="password_confirmation" placeholder="もう一度パスワードを入力">
           <x-password-confirm-error class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
             passwordMessage="パスワードと一致しません" />
         </div>
 
         <!-- 送信ボタン -->
-        <div class="register-form__button">
-          <button class="register-form__button-submit" type="submit">登録する</button>
+        <div class="register-page__button">
+          <button class="register-page__button-submit" type="submit">登録する</button>
         </div>
       </form>
 
       <!-- リンク -->
-      <div class="register-form__link">
-        <a href="/login" class="register-form__link--login">ログインはこちら</a>
+      <div class="register-page__link">
+        <a href="/login" class="register-page__link--login">ログインはこちら</a>
       </div>
     </div>
   </div>
