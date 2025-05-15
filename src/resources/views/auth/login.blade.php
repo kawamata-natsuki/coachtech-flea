d902b4726a78401dab5867863c95f9cc0b0467fc
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-15 16:21:13 +0900

    chore：ログインページのレイアウト調整・コード整理

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,46 +5,47 @@
 @endsection
 
 @section('content')
-<div class="login-form">
-  <div class="login-form__container">
-    <h1 class="login-form__heading content__heading">
+<div class="login-page">
+  <div class="login-page__container">
+    <h1 class="login-page__heading content__heading">
       ログイン
     </h1>
 
-    <div class="login-form__content">
-      <form class="login-form__form" action="/login" method="post" novalidate>
+    <div class="login-page__content">
+      <form class="login-page__form" action="/login" method="post" novalidate>
         @csrf
-        <!-- バリデーションエラー（ログイン失敗） -->
-        <div class="login-form__group">
+
+        <!-- ログイン失敗時のエラー -->
+        <div class="login-page__form-group">
           <x-error-message field="login" class="error-message {{ $errors->has('login') ? 'has-error' : 'no-error' }}" />
         </div>
 
         <!-- メールアドレス -->
-        <div class="login-form__group">
-          <label class="login-form__label form__label" for="email">メールアドレス</label>
-          <input class="login-form__input form__input" type="email" name="email" id="email" value="{{ old('email') }}"
+        <div class="login-page__form-group">
+          <label class="login-page__label form__label" for="email">メールアドレス</label>
+          <input class="login-page__input form__input" type="email" name="email" id="email" value="{{ old('email') }}"
             placeholder="例：user@example.com">
           <x-error-message field="email" class="error-message {{ $errors->has('email') ? 'has-error' : 'no-error' }}" />
         </div>
 
-
         <!-- パスワード -->
-        <div class="login-form__group">
-          <label class="login-form__label form__label" for="password">パスワード</label>
-          <input class="login-form__input form__input" type="password" name="password" id="password"
+        <div class="login-page__form-group">
+          <label class="login-page__label form__label" for="password">パスワード</label>
+          <input class="login-page__input form__input" type="password" name="password" id="password"
             placeholder="8文字以上のパスワードを入力">
           <x-error-message field="password"
             class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}" />
         </div>
 
-
         <!-- 送信ボタン -->
-        <div class="login-form__button">
-          <button class="login-form__button-submit" type="submit">ログインする</button>
+        <div class="login-page__button">
+          <button class="login-page__button-submit" type="submit">ログインする</button>
         </div>
       </form>
-      <div class="login-form__link">
-        <a href="/register" class="login-form__link--register">会員登録はこちら</a>
+
+      <!-- リンク -->
+      <div class="login-page__link">
+        <a href="/register" class="login-page__link--register">会員登録はこちら</a>
       </div>
     </div>
   </div>
