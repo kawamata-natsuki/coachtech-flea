ed5412c6fba408f7884eced2d34f83a3d371651e
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-16 00:20:28 +0900

    chore：クラス名の整理

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -15,39 +15,42 @@
       <form class="register-page__form" action="/register" method="post" novalidate>
         @csrf
 
-        <!-- ユーザー名 -->
-        <div class="register-page__form-group">
-          <label class="register-page__label form__label" for="name">ユーザー名</label>
-          <input class="form__input register-page__input" type="text" name="name" id="name" value="{{ old('name') }}"
-            placeholder="例：山田　太郎">
-          <x-error-message field="name" class="error-message {{ $errors->has('name') ? 'has-error' : 'no-error' }}" />
-        </div>
+        <div class="register-page__form-section">
+          <!-- ユーザー名 -->
+          <div class="register-page__form-group">
+            <label class="register-page__label form__label" for="name">ユーザー名</label>
+            <input class="form__input register-page__input" type="text" name="name" id="name" value="{{ old('name') }}"
+              placeholder="例：山田　太郎">
+            <x-error-message field="name" class="error-message {{ $errors->has('name') ? 'has-error' : 'no-error' }}" />
+          </div>
 
-        <!-- メールアドレス -->
-        <div class="register-page__form-group">
-          <label class="register-page__label form__label" for="email">メールアドレス</label>
-          <input class="form__input register-page__input" type="email" name="email" id="email"
-            value="{{ old('email') }}" placeholder="例：user@example.com">
-          <x-error-message field="email" class="error-message {{ $errors->has('email') ? 'has-error' : 'no-error' }}" />
-        </div>
+          <!-- メールアドレス -->
+          <div class="register-page__form-group">
+            <label class="register-page__label form__label" for="email">メールアドレス</label>
+            <input class="form__input register-page__input" type="email" name="email" id="email"
+              value="{{ old('email') }}" placeholder="例：user@example.com">
+            <x-error-message field="email"
+              class="error-message {{ $errors->has('email') ? 'has-error' : 'no-error' }}" />
+          </div>
 
-        <!-- パスワード -->
-        <div class="register-page__form-group">
-          <label class="register-page__label form__label" for="password">パスワード</label>
-          <input class="form__input register-page__input" type="password" name="password" id="password"
-            placeholder="8文字以上のパスワードを入力">
-          <x-error-message field="password"
-            class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
-            :excludeMessage="'パスワードと一致しません'" />
-        </div>
+          <!-- パスワード -->
+          <div class="register-page__form-group">
+            <label class="register-page__label form__label" for="password">パスワード</label>
+            <input class="form__input register-page__input" type="password" name="password" id="password"
+              placeholder="8文字以上のパスワードを入力">
+            <x-error-message field="password"
+              class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
+              :excludeMessage="'パスワードと一致しません'" />
+          </div>
 
-        <!-- 確認用パスワード -->
-        <div class="register-page__form-group">
-          <label class="register-page__label form__label" for="password_confirmation">確認用パスワード</label>
-          <input class="form__input register-page__input" type="password" name="password_confirmation"
-            id="password_confirmation" placeholder="もう一度パスワードを入力">
-          <x-password-confirm-error class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
-            passwordMessage="パスワードと一致しません" />
+          <!-- 確認用パスワード -->
+          <div class="register-page__form-group">
+            <label class="register-page__label form__label" for="password_confirmation">確認用パスワード</label>
+            <input class="form__input register-page__input" type="password" name="password_confirmation"
+              id="password_confirmation" placeholder="もう一度パスワードを入力">
+            <x-password-confirm-error class="error-message {{ $errors->has('password') ? 'has-error' : 'no-error' }}"
+              passwordMessage="パスワードと一致しません" />
+          </div>
         </div>
 
         <!-- 送信ボタン -->
@@ -55,12 +58,13 @@
           <button class="register-page__button-submit" type="submit">登録する</button>
         </div>
       </form>
+    </div>
 
-      <!-- リンク -->
-      <div class="register-page__link">
-        <a href="/login" class="register-page__link--login">ログインはこちら</a>
-      </div>
+    <!-- リンク -->
+    <div class="register-page__link">
+      <a href="/login" class="register-page__link--login">ログインはこちら</a>
     </div>
   </div>
+</div>
 </div>
 @endsection
