ef0d35a387fa52009be31ad418418dd5d9559651
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-17 02:05:35 +0900

    refactor：protected $dontFlash から current_password を削除

--- src/app/Exceptions/Handler.php
+++ src/app/Exceptions/Handler.php
@@ -7,21 +7,21 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /* バリデーションエラーでフォームが再表示されたときに、値を"フラッシュしない"（=再表示しない）入力項目を指定する */
+    /** バリデーションエラー時に再表示しない入力項目を指定 */
     protected $dontFlash = [
-        'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /* エラーが発生したときに、どの画面を表示するかを決める */
+    /** エラー発生時の画面遷移 */
     public function render($request, Throwable $exception)
     {
-        /* CSRFトークンエラー（TokenMismatchException）の時の処理 */
+        /** CSRFトークンエラー（TokenMismatchException）の処理 */
         if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
             return redirect()->route('login')->with('error', 'セッションの有効期限が切れました。もう一度ログインしてください。');
         }
-        /* 上記条件に該当しない場合は、Laravelデフォルトのエラー表示 */
+
+        /*その他の例外は、Laravelデフォルトのエラー表示 */
         return parent::render($request, $exception);
     }
 }
