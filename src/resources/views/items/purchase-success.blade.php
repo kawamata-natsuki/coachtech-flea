0c14024420f7bd261c4e0326dfcd1e77523a2e2c
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-16 16:43:50 +0900

    chore：レイアウト調整・コードの整理

--- src/resources/views/items/purchase-success.blade.php
+++ src/resources/views/items/purchase-success.blade.php
@@ -5,20 +5,20 @@
 @endsection
 
 @section('content')
-<div class="purchase-success">
-  <div class="purchase-success__container">
-    <h1 class="purchase-success__title">ご購入ありがとうございます！</h1>
-    <p class="purchase-success__message">
+<div class="purchase-success-page">
+  <div class="purchase-success-page__container">
+    <h1 class="purchase-success-page__title">ご購入ありがとうございます！</h1>
+    <p class="purchase-success-page__message">
       商品のご購入が完了しました。<br>
       発送までしばらくお待ちください。
     </p>
 
-    <div class="purchase-success__actions">
-      <a href="{{ route('items.index') }}" class="purchase-success__button purchase-success__button--primary">
+    <div class="purchase-success-page__buttons">
+      <a href="{{ route('items.index') }}" class="purchase-success-page__button purchase-success-page__button--primary">
         トップページへ戻る
       </a>
       <a href="{{ route('profile.index', ['page' => 'buy']) }}"
-        class="purchase-success__button purchase-success__button--secondary">
+        class="purchase-success-page__button purchase-success-page__button--secondary">
         購入履歴を見る
       </a>
     </div>
