cd83afa004e038dafca42b022f5cb46052c28f6a
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-16 21:50:59 +0900

    chore：レイアウト調整・コードの整理

--- src/resources/views/items/purchase-cancel.blade.php
+++ src/resources/views/items/purchase-cancel.blade.php
@@ -5,16 +5,16 @@
 @endsection
 
 @section('content')
-<div class="purchase-cancel">
-  <div class="purchase-cancel__container">
-    <h1 class="purchase-cancel__title">購入手続きがキャンセルされました</h1>
-    <p class="purchase-cancel__message">
+<div class="purchase-cancel-page">
+  <div class="purchase-cancel-page__container">
+    <h1 class="purchase-cancel-page__title">購入手続きがキャンセルされました</h1>
+    <p class="purchase-cancel-page__message">
       ご注文は完了していません。<br>
       再度お試しいただくか、トップページにお戻りください。
     </p>
 
-    <div class="purchase-cancel__actions">
-      <a href="{{ route('items.index') }}" class="purchase-cancel__button purchase-cancel__button--primary">
+    <div class="purchase-cancel-page__button">
+      <a href="{{ route('items.index') }}" class="purchase-cancel-page__button--primary">
         商品一覧に戻る
       </a>
     </div>
---
+++ src/resources/views/items/purchase-invalid.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/purchase-invalid.css') }}">
+@endsection
+
+@section('content')
+<div class="purchase-invalid-page">
+  <div class="purchase-invalid-page__container">
+    <h1 class="purchase-invalid-page__title">購入手続きがキャンセルされました</h1>
+    <p class="purchase-invalid-page__message">
+      この商品はすでに購入済み、または売り切れのため注文できません。<br>
+      他の商品をご覧いただくか、トップページにお戻りください。
+    </p>
+
+    <div class="purchase-invalid-page__button">
+      <a href="{{ route('items.index') }}" class="purchase-invalid-page__button--primary">
+        商品一覧に戻る
+      </a>
+    </div>
+  </div>
+</div>
+@endsection
