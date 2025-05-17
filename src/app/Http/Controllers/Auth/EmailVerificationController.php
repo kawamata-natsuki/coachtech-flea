eeefc8fde0395a2c6b7b381a6403b88e01981d21
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-17 19:37:39 +0900

    chore：コメント整理

--- src/app/Http/Controllers/Auth/EmailVerificationController.php
+++ src/app/Http/Controllers/Auth/EmailVerificationController.php
@@ -8,24 +8,27 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 class EmailVerificationController extends Controller
 {
+    /** メール認証画面の表示 */
     public function notice()
     {
         return view('auth.verify-email');
     }
 
+    /** メール認証の処理 */
     public function verify(EmailVerificationRequest $request)
     {
         $request->fulfill();
         return redirect('/mypage/profile');
     }
 
+    /** 認証メール再送信 */
     public function resend(Request $request)
     {
-        /** メール認証の再送信 */
         $request->user()->sendEmailVerificationNotification();
         return back()->with('message', '確認メールを再送しました');
     }
 
+    /** メール認証済みか確認してリダイレクト */
     public function check()
     {
         return redirect(auth()->user()?->hasVerifiedEmail() ? '/' : '/email/verify');
