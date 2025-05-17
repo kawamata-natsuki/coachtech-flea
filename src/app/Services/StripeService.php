12c4d90304ffe5cc8c16bb7910a464e743ef53a4
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-18 02:26:07 +0900

    chore：コメント整理

--- src/app/Services/StripeService.php
+++ src/app/Services/StripeService.php
@@ -8,16 +8,20 @@ use Stripe\Checkout\Session;
 
 class StripeService
 {
+    // 指定された商品と支払い方法に基づいて、Stripeの支払いセッションを作成
+    // 作成されたセッションURLを返す
     public function createCheckoutSession(Item $item, string $paymentMethod): string
     {
         Stripe::setApiKey(config('services.stripe.secret'));
 
+        // 支払い方法コードに応じてStripeの決済タイプを判定
         $methodType = match ($paymentMethod) {
             'credit_card' => 'card',
             'convenience_store' => 'konbini',
             default => throw new \InvalidArgumentException('不正な支払い方法です'),
         };
 
+        // StripeのCheckoutセッションを作成
         $session = Session::create([
             'payment_method_types' => [$methodType],
             'line_items' => [[
