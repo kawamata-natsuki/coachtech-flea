7513116567f5dfe95d3d754b5b20edd638661cf8
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-17 13:24:13 +0900

    chore：不要なコメントアウトとuse文削除

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Casts\CategoryCodeCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,7 +11,7 @@ class Category extends Model
 
     protected $fillable = ['name'];
 
-    // リレーション
+    /** リレーション */
     public function items()
     {
         return $this->belongsToMany(
