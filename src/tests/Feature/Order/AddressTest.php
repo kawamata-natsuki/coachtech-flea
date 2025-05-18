7ea403814e75ab4d12f841ef78b633cdac950524
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-18 22:42:38 +0900

    chore：コメントの整理

--- src/tests/Feature/Order/AddressTest.php
+++ src/tests/Feature/Order/AddressTest.php
@@ -2,17 +2,20 @@
 
 namespace Tests\Feature;
 
-use App\Models\Item;
+use App\Constants\PaymentMethodConstants;
 use App\Models\PaymentMethod;
-use App\Models\User;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\ConditionSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\TestHelpers\AuthTestHelper;
+use Tests\TestHelpers\ItemTestHelper;
 
 class AddressTest extends TestCase
 {
     use RefreshDatabase;
+    use AuthTestHelper;
+    use ItemTestHelper;
 
     protected string $paymentMethodCode;
 
@@ -21,31 +24,27 @@ class AddressTest extends TestCase
         parent::setUp();
         $this->seed(CategorySeeder::class);
         $this->seed(ConditionSeeder::class);
-
-        $this->paymentMethodCode = 'credit_card';
+        $this->paymentMethodCode = PaymentMethodConstants::CREDIT_CARD;
 
         // テスト用の支払方法を登録
         PaymentMethod::create([
-            'code' => 'credit_card',
-            'name' => 'カード支払い',
+            'code' => PaymentMethodConstants::CREDIT_CARD,
+            'name' => PaymentMethodConstants::label(PaymentMethodConstants::CREDIT_CARD),
         ]);
     }
 
+    /**
+     * 送付先住所変更画面にて登録した住所が商品購入画面に反映されている
+     */
     public function test_registered_address_is_reflected_on_purchase_screen()
-    // 送付先住所変更画面にて登録した住所が商品購入画面に反映されている
     {
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create();
+        // ログインユーザーと商品データを作成
+        $user = $this->loginUser();
+        $item = $this->createItem();
 
-        $item = Item::factory()->create();
-
-        // 1. ユーザーにログインする
-        $this->actingAs($user);
-
-        // 2. 送付先住所変更画面で住所を登録する 
+        // 送付先住所変更画面で住所を登録する 
         $response = $this->get(route('address.edit', ['item' => $item->id]));
         $response->assertStatus(200);
-
         $addressData = [
             'postal_code' => '130-0001',
             'address' => '東京都墨田区吾妻橋1-23-1',
@@ -53,7 +52,7 @@ class AddressTest extends TestCase
         ];
         $this->put(route('address.update', ['item' => $item->id]), $addressData);
 
-        // 3. 商品購入画面を再度開く
+        // 商品購入画面を再度開く
         $response = $this->get(route('purchase.show', ['item' => $item->id]));
 
         // 登録した住所が商品購入画面に正しく反映される
@@ -62,20 +61,18 @@ class AddressTest extends TestCase
         $response->assertSee('アサヒビール1F');
     }
 
+    /**
+     * 購入した商品に送付先住所が紐づいて登録される
+     */
     public function test_address_is_stored_with_purchased_item()
-    // 購入した商品に送付先住所が紐づいて登録される
     {
-        /** @var \App\Models\User */
-        $user = User::factory()->create();
-        $item = Item::factory()->create();
+        // ログインユーザーと商品データを作成
+        $user = $this->loginUser();
+        $item = $this->createItem();
 
-        // 1. ユーザーにログインする
-        $this->actingAs($user);
-
-        // 2. 送付先住所変更画面で住所を登録する 
+        // 送付先住所変更画面で住所を登録する 
         $response = $this->get(route('address.edit', ['item' => $item->id]));
         $response->assertStatus(200);
-
         $addressData = [
             'postal_code' => '130-0001',
             'address' => '東京都墨田区吾妻橋1-23-1',
@@ -83,10 +80,9 @@ class AddressTest extends TestCase
         ];
         $this->put(route('address.update', ['item' => $item->id]), $addressData);
 
-        // 3. 商品を購入する
+        // 商品を購入する
         $response = $this->get(route('purchase.show', ['item' => $item->id]));
         $response->assertStatus(200);
-
         $response = $this
             ->withSession(['purchase.payment_method' =>  $this->paymentMethodCode])
             ->get(route('purchase.success', ['item' => $item->id]));
