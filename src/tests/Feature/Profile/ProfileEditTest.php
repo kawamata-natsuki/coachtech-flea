97efa8e2970dedd4102d95f01cc67e25126be589
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-18 23:10:24 +0900

    chore：コメントの整理

--- src/tests/Feature/Profile/ProfileEditTest.php
+++ src/tests/Feature/Profile/ProfileEditTest.php
@@ -2,33 +2,36 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
+use Tests\TestHelpers\AuthTestHelper;
 
 class ProfileEditTest extends TestCase
 {
     use RefreshDatabase;
+    use AuthTestHelper;
 
+    /**
+     * 変更項目が初期値として過去設定されていること（プロフィール画像、ユーザー名、郵便番号、住所）
+     */
     public function test_user_can_update_profile_information()
-    // 変更項目が初期値として過去設定されていること（プロフィール画像、ユーザー名、郵便番号、住所）
     {
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create([
+        // 初期データを持つユーザーを作成し、ログインする
+        $user = $this->createUser([
             'name' => '旧ユーザー名',
             'postal_code' => '100-0000',
             'address' => '旧住所',
             'building' => '旧建物',
             'profile_image' => 'profile_images/dummy.jpg',
         ]);
-
         $this->actingAs($user);
 
+        // プロフィール編集画面を開く
         $response = $this->get(route('profile.edit'));
         $response->assertStatus(200);
 
+        // 新しいプロフィール情報を入力し、更新する
         $newData = [
             'name' => '新ユーザー名',
             'postal_code' => '123-4567',
@@ -36,11 +39,11 @@ class ProfileEditTest extends TestCase
             'building' => '新建物',
             'profile_image' => UploadedFile::fake()->image('new_dummy.jpg'),
         ];
-
         $response = $this->put(route('profile.update'), $newData);
         $response->assertRedirect(route('profile.edit'));
         $response->assertSessionHas('success');
 
+        // プロフィールページを表示して、変更内容を確認する
         $response = $this->get(route('profile.edit'));
         $response->assertStatus(200);
         $response->assertSee('新ユーザー名');
@@ -49,6 +52,7 @@ class ProfileEditTest extends TestCase
         $response->assertSee('新建物');
         $response->assertSee($user->fresh()->profile_image);
 
+        //  DBに変更が保存されたことを確認
         $this->assertDatabaseHas('users', [
             'name' => '新ユーザー名',
             'postal_code' => '123-4567',
