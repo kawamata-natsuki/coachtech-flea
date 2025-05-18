9bbe34dfaadd85ecbdf6602b8f7ca67d9eca2643
kawamata natsuki <t.natsuki020817@gmail.com>
2025-05-18 22:18:17 +0900

    chore：コメントの整理

--- src/tests/Feature/Item/MyListTest.php
+++ src/tests/Feature/Item/MyListTest.php
@@ -8,10 +8,14 @@ use App\Models\Item;
 use Database\Seeders\ConditionSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Tests\TestHelpers\AuthTestHelper;
+use Tests\TestHelpers\ItemTestHelper;
 
 class MyListTest extends TestCase
 {
     use RefreshDatabase;
+    use AuthTestHelper;
+    use ItemTestHelper;
 
     protected function setUp(): void
     {
@@ -19,92 +23,90 @@ class MyListTest extends TestCase
         $this->seed(ConditionSeeder::class);
     }
 
+    /**
+     * いいねした商品だけが表示される
+     */
     public function test_only_favorited_items_are_displayed_in_mylist_tab()
     {
         // ログインユーザーを作成
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create();
+        $user = $this->loginUser();
 
-        // 商品を複数作成
-        $itemLiked = Item::factory()->create([
+        // 複数商品を作成、 $itemLiked にいいねする
+        $itemLiked = $this->createItem([
             'name' => 'LikedItem',
         ]);
-        $itemNotLiked = Item::factory()->create([
+        $itemNotLiked = $this->createItem([
             'name' => 'NotLikedItem',
         ]);
-
-        // いいね登録
         $user->favoriteItems()->attach($itemLiked->id);
 
-        // 1. ユーザーにログインをする
-        $this->actingAs($user);
-
-        // 2. マイリストページを開く
+        // マイリストページを開く
         $response = $this->get('/?page=mylist');
         $response->assertStatus(200);
 
-        // いいねした商品だけが表示される
+        // いいねをした商品が表示される
         $response->assertSee('LikedItem');
         $response->assertDontSee('NotLikedItem');
     }
 
+    /**
+     * 購入済み商品は「Sold」と表示される
+     */
     public function test_sold_label_is_displayed_for_sold_items_in_my_list_tab()
     {
         // ログインユーザーを作成
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create();
+        $user = $this->loginUser();
 
-        // 売り切れ商品を作成
-        $soldItem = Item::factory()->create([
+        // 売り切れ商品を作成し、いいねをする
+        $soldItem = $this->createItem([
             'name' => 'testA',
             'item_status' => ItemStatus::SOLD_OUT,
         ]);
-
-        // いいね登録
         $user->favoriteItems()->attach($soldItem->id);
 
-        // 1. ユーザーにログインをする
-        $this->actingAs($user);
-
-        // 2. マイリストページを開く
+        // マイリストページを開く
         $response = $this->get('/?page=mylist');
         $response->assertStatus(200);
 
-        // 3. 購入済み商品を確認する
+        // 購入済み商品を確認する
         $response->assertSee('testA');
 
-        // 購入済み商品は「Sold」と表示される
+        // 購入済み商品に「Sold」のラベルが表示される
         $response->assertSee('item-card__sold-label');
     }
 
+    /**
+     * 自分が出品した商品は表示されない
+     */
     public function test_items_created_by_user_are_not_displayed_in_mylist_tab()
     {
         // ログインユーザーを作成
-        /** @var \App\Models\User $user */
-        $user = User::factory()->create();
+        $user = $this->loginUser();
 
-        // ログインユーザーが出品した商品を作成
-        // いいね登録
-        $myItem = Item::factory()->create([
+        // ログインユーザーが出品した商品を作成し、いいねする
+        $myItem = $this->createItem([
             'name' => 'MyItem',
             'user_id' => $user->id,
         ]);
-
         $user->favoriteItems()->attach($myItem->id);
 
-        // ログイン＆アクセス
-        $this->actingAs($user);
+        // マイリストページを開く
         $response = $this->get('/?page=mylist');
         $response->assertStatus(200);
 
-        // 自分が出品した商品は表示されない
+        // 自分が出品した商品が一覧に表示されない
         $response->assertDontSee('MyItem');
     }
 
+    /**
+     * 未認証の場合は何も表示されない
+     */
     public function test_no_items_are_displayed_in_mylist_tab_when_not_authenticated()
     {
-        // 未認証の場合は何も表示されない
+        // ゲストユーザー
+        auth()->logout();
 
+        // 何も表示されない
         $response = $this->get('/?page=mylist');
         $response->assertStatus(200);
         $response->assertSee('表示する商品がありません');
